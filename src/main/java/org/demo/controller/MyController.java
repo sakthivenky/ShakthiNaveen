ae484014c5b7de9e86aebf9592f28e7091fe5815
ararumug <arulkumar.arumugam@aig.com>
2020-08-08 21:47:01 +0530

    first commit
    first commit

---
+++ src/main/java/org/demo/controller/MyController.java
@@ -0,0 +1,20 @@
+package org.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/xyz")
+public class MyController {
+	
+	@RequestMapping(value="/mnop", method=RequestMethod.GET, produces="text/html")
+	public String test(
+			@RequestParam (name="myname") String mname			
+			)
+	{
+		return "Hi Hello Welcome to SpringBoot Mr."+mname;
+	}
+
+}
